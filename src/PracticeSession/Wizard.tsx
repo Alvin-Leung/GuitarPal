bd9781100c8053fe90ad92f6298a85897c5871ca
alvin <alvin.leung92@gmail.com>
2019-05-12 23:05:22 -0700

    Add practice items column and start button to Wizard component

--- src/PracticeSession/Wizard.tsx
+++ src/PracticeSession/Wizard.tsx
@@ -1,12 +1,48 @@
 import React from "react";
 import { CardColumn } from "./CardColumn/CardColumn";
-import { Row } from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
+import { PracticeItemCard } from "./CardColumn/PracticeItemCard";
+import { Button, Intent } from "@blueprintjs/core";
 
 export class Wizard extends React.Component {
+  private totalPracticeMinutes: number = 50;
+
   render() {
     return (
       <Row className="mt-3">
         <CardColumn />
+        <Col xs={4}>
+          <Row>
+            <Col xs={6}>
+              <span className="btn btn-sm">
+                <b>2</b> Practice Items
+              </span>
+            </Col>
+            <Col xs={6}>
+              <span className="float-right btn btn-sm">
+                <em>Total Time: {this.totalPracticeMinutes} mins</em>
+              </span>
+            </Col>
+          </Row>
+
+          <hr />
+
+          <PracticeItemCard
+            title="Learn Purple Haze"
+            description="Play along with song and nail solo"
+            timeIntervalInMinutes={30}
+          />
+          <PracticeItemCard
+            title="Increase picking speed"
+            description="Practice Paul Gilbert speed picking exericse"
+            timeIntervalInMinutes={30}
+          />
+        </Col>
+        <Col xs={4}>
+          <Button intent={Intent.SUCCESS} icon="arrow-right">
+            Start Practice
+          </Button>
+        </Col>
       </Row>
     );
   }
