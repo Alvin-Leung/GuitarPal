c936deca803bf0310e4004cdbb4633cdaa9552da
alvin <alvin.leung92@gmail.com>
2019-05-12 21:26:49 -0700

    Extend GoalCard with AbstractCard

--- src/PracticeSession/CardColumn/GoalCard.tsx
+++ src/PracticeSession/CardColumn/GoalCard.tsx
@@ -1,34 +1,27 @@
 import React from "react";
-import {
-  Button,
-  ProgressBar,
-  Intent,
-  Elevation,
-  Card
-} from "@blueprintjs/core";
+import { ProgressBar, Intent, Elevation, Card } from "@blueprintjs/core";
+import { ICardProps } from "./Interfaces";
+import { AbstractCard } from "./AbstractCard";
 
-type Props = {
-    title: string;
-    description: string;
-    progress: number;
+export interface IGoalCardProps extends ICardProps {
+  progress: number;
 }
 
-export class GoalCard extends React.Component<Props> {
-  render = () => {
+export class GoalCard extends AbstractCard<IGoalCardProps> {
+  generateCardContent(): React.ReactNode {
     return (
-      <Card interactive={true} elevation={Elevation.TWO} className="mt-2">
+      <div>
         <h5>
           <a href="#">{this.props.title}</a>
         </h5>
         <p>{this.props.description}</p>
-        <Button>Details</Button>
         <ProgressBar
           intent={Intent.SUCCESS}
           stripes={false}
           value={this.props.progress}
           className="mt-3"
         />
-      </Card>
+      </div>
     );
   }
 }
