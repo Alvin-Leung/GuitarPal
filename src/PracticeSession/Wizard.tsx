942948367e3702dcdbc53ed015805ac2e415e681
alvin <alvin.leung92@gmail.com>
2019-05-25 11:38:29 -0700

    Change key member to id, and ensure this id is assigned to component key props.

--- src/PracticeSession/Wizard.tsx
+++ src/PracticeSession/Wizard.tsx
@@ -7,34 +7,34 @@ import { IPracticeItemCardProps, PracticeItemCard } from "./CardColumn/PracticeI
 import { PracticeItemCardColumn } from "./CardColumn/PracticeItemCardColumn";
 
 interface IPracticeItem extends IPracticeItemCardProps {
-  key: string;
-  practiceTime: Date;
+  id: string;
 }
 
 interface IGoal extends IGoalCardProps {
-  key: any;
+  id: any;
 }
 
 interface State {
-  readonly practiceItems: IPracticeItem[]
+  readonly practiceItems: IPracticeItem[],
+  readonly practiceTimes: Date[]
 }
 
 export class Wizard extends React.Component<any, State> {
   private goalCards: IGoal[] = [
     {
-      key: "8b71a16a-9274-417e-8c47-65ac971f29b4",
+      id: "8b71a16a-9274-417e-8c47-65ac971f29b4",
       title: "Learn Dorian scale",
       description: "Master the dorian scale in 12 keys",
       progress: 0.75
     },
     {
-      key: "74085497-c36a-43b5-8d5e-ecc1d3aeec28",
+      id: "74085497-c36a-43b5-8d5e-ecc1d3aeec28",
       title: "Improvise",
       description: "Improve improvisation chops over high-tempo funk track",
       progress: 0.1
     },
     {
-      key: "38d9d028-97f7-4336-a01f-fe6837e53674",
+      id: "38d9d028-97f7-4336-a01f-fe6837e53674",
       title: "Compose",
       description: "Finish composing ballad 'I Love My Guitar'",
       progress: 0
@@ -45,24 +45,22 @@ export class Wizard extends React.Component<any, State> {
     super(props);
 
     this.state = {
+      practiceTimes: [],
       practiceItems: [
         {
-          key: "65979da7-41df-4c73-a7f3-dbdaa5910ebe",
+          id: "65979da7-41df-4c73-a7f3-dbdaa5910ebe",
           title: "Learn Purple Haze",
-          description: "Play along with song and nail solo",
-          practiceTime: new Date(0)
+          description: "Play along with song and nail solo"
         },
         {
-          key: "0ebe0ad0-5fbd-494a-bc0a-09205e9d590e",
+          id: "0ebe0ad0-5fbd-494a-bc0a-09205e9d590e",
           title: "Increase picking speed",
-          description: "Practice Paul Gilbert speed picking exericse",
-          practiceTime: new Date(0)
+          description: "Practice Paul Gilbert speed picking exericse"
         },
         {
-          key: "184e0b44-aa5b-4789-9cda-9abc7acf5674",
+          id: "184e0b44-aa5b-4789-9cda-9abc7acf5674",
           title: "Compose",
-          description: "Compose that new song",
-          practiceTime: new Date(0)
+          description: "Compose that new song"
         }
       ]
     };
@@ -72,14 +70,14 @@ export class Wizard extends React.Component<any, State> {
     return (
       <Row className="mt-3">
         <GoalCardColumn bootstrapColumnWidth={3}>
-          {this.goalCards.map(props => (
-            <GoalCard {...props} />
+          {this.goalCards.map(goal => (
+            <GoalCard key={goal.id} {...goal} />
           ))}
         </GoalCardColumn>
 
         <PracticeItemCardColumn totalPracticeMinutes={51} bootstrapColumnWidth={4}>
-          {this.state.practiceItems.map(props => (
-            <PracticeItemCard {...props} />
+          {this.state.practiceItems.map(practiceItem => (
+            <PracticeItemCard key={practiceItem.id} {...practiceItem} />
           ))}
         </PracticeItemCardColumn>
 
