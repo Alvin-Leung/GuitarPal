cb6bff2b5b2d3e787ff2534bcd85555cf85bb639
alvin <alvin.leung92@gmail.com>
2019-06-01 16:37:42 -0700

    Pass ids for edit instead of entire practice item objects

--- src/PracticeSession/Wizard.tsx
+++ src/PracticeSession/Wizard.tsx
@@ -9,16 +9,19 @@ import { PracticeItemCardColumn } from "./CardColumn/PracticeItemCardColumn";
 import { ITotalTime, TotalTimeBuilder } from "./TotalTimeBuilder";
 import { EditPracticeItemDialog } from "./EditPracticeItemDialog";
 
+interface IPracticeItemLookup {
+  [practiceItemId: string]: ICardProps;
+}
+
 interface IPracticeTimeLookup {
   [practiceItemId: string]: Date;
 }
 
 interface State {
-  readonly practiceItems: ICardProps[];
+  readonly practiceItems: IPracticeItemLookup;
   readonly practiceTimes: IPracticeTimeLookup;
   readonly isEditMode: boolean;
-  // TODO: Instead of practiceItemToEdit property, hold only id of practice item to edit
-  readonly practiceItemToEdit?: ICardProps;
+  readonly editItemId?: string;
 }
 
 export class Wizard extends React.Component<any, State> {
@@ -46,25 +49,15 @@ export class Wizard extends React.Component<any, State> {
   public constructor(props: any) {
     super(props);
 
+    const practiceItems: IPracticeItemLookup = {};
+    
+    this.getPracticeItems().forEach(item => {
+      practiceItems[item.id] = item;
+    });
+
     this.state = {
       practiceTimes: {},
-      practiceItems: [
-        {
-          id: "65979da7-41df-4c73-a7f3-dbdaa5910ebe",
-          title: "Learn Purple Haze",
-          description: "Play along with song and nail solo"
-        },
-        {
-          id: "0ebe0ad0-5fbd-494a-bc0a-09205e9d590e",
-          title: "Increase picking speed",
-          description: "Practice Paul Gilbert speed picking exericse"
-        },
-        {
-          id: "184e0b44-aa5b-4789-9cda-9abc7acf5674",
-          title: "Compose",
-          description: "Compose that new song"
-        }
-      ],
+      practiceItems: practiceItems,
       isEditMode: false
     };
   }
@@ -83,13 +76,13 @@ export class Wizard extends React.Component<any, State> {
             totalPracticeTime={this.getTotalPracticeTime()}
             bootstrapColumnWidth={4}
           >
-            {this.state.practiceItems.map(practiceItem => (
+            {Object.keys(this.state.practiceItems).map(key => (
               <PracticeItemCard
-                key={practiceItem.id}
-                practiceTime={this.state.practiceTimes[practiceItem.id]}
+                key={key}
+                practiceTime={this.state.practiceTimes[key]}
                 onTimeChange={this.onPracticeTimeChange}
                 onEdit={this.onEditPracticeItem}
-                {...practiceItem}
+                {...this.state.practiceItems[key]}
               />
             ))}
           </PracticeItemCardColumn>
@@ -100,16 +93,43 @@ export class Wizard extends React.Component<any, State> {
             </Button>
           </Col>
         </Row>
-        {this.state.practiceItemToEdit && <EditPracticeItemDialog isOpen={this.state.isEditMode} practiceCard={this.state.practiceItemToEdit} onSave={this.onSavePracticeItem} onCloseOrCancel={this.onCloseOrCancel} />}
+        {this.state.editItemId && (
+          <EditPracticeItemDialog
+            isOpen={this.state.isEditMode}
+            practiceCard={this.state.practiceItems[this.state.editItemId]}
+            onSave={this.onSavePracticeItem}
+            onCloseOrCancel={this.onCloseOrCancel}
+          />
+        )}
       </div>
     );
+  }
+
+  private getPracticeItems(): ICardProps[] {
+    return [
+      {
+        id: "65979da7-41df-4c73-a7f3-dbdaa5910ebe",
+        title: "Learn Purple Haze",
+        description: "Play along with song and nail solo"
+      },
+      {
+        id: "0ebe0ad0-5fbd-494a-bc0a-09205e9d590e",
+        title: "Increase picking speed",
+        description: "Practice Paul Gilbert speed picking exericse"
+      },
+      {
+        id: "184e0b44-aa5b-4789-9cda-9abc7acf5674",
+        title: "Compose",
+        description: "Compose that new song"
+      }
+    ];
   }
 
   private getTotalPracticeTime(): ITotalTime {
     const totalTimeBuilder = new TotalTimeBuilder();
 
-    this.state.practiceItems.forEach(practiceItem => {
-      totalTimeBuilder.AddTime(this.state.practiceTimes[practiceItem.id]);
+    Object.keys(this.state.practiceItems).forEach(key => {
+      totalTimeBuilder.AddTime(this.state.practiceTimes[key]);
     });
 
     return totalTimeBuilder.Build();
@@ -124,26 +144,23 @@ export class Wizard extends React.Component<any, State> {
   };
 
   private onEditPracticeItem = (id: string) => {
-    // TODO: Make practice items searchable by string id (index signatures)
-    const practiceItemToEdit = this.state.practiceItems.find(item => item.id == id);
-
     this.setState({
       isEditMode: true,
-      practiceItemToEdit: practiceItemToEdit
+      editItemId: id
     });
-  }
+  };
 
   private onSavePracticeItem = (practiceItem: ICardProps) => {
     // TODO: Save edited practice item to state
     console.log(practiceItem);
     // TODO: On success, show toast
     this.onCloseOrCancel();
-  }
+  };
 
   private onCloseOrCancel = () => {
-    console.log('closed or cancelled');
+    console.log("closed or cancelled");
     this.setState({
       isEditMode: false
-    })
-  }
+    });
+  };
 }
