2b287e74ae70bb251e44089352236eb29670d1e8
alvin <alvin.leung92@gmail.com>
2019-06-02 20:10:41 -0700

    Implement exit confirmation dialog when the user has made changes in edit mode

--- src/PracticeSession/EditPracticeItemDialog.tsx
+++ src/PracticeSession/EditPracticeItemDialog.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Dialog, EditableText, Button, Intent } from "@blueprintjs/core";
+import { Dialog, EditableText, Button, Intent, Alert } from "@blueprintjs/core";
 import { Row, Col } from "react-bootstrap";
 import "./EditPracticeItemDialog.css";
 import { IconNames } from "@blueprintjs/icons";
@@ -14,19 +14,24 @@ export interface Props {
 
 export interface State {
   practiceCard: ICardProps;
+  openExitConfirmationDialog: boolean;
 }
 
 export class EditPracticeItemDialog extends React.Component<Props, State> {
   // TODO: Explictly declare access modifiers to methods
   handleDialogOpening = () => {
     this.setState({
-      practiceCard: this.props.initialPracticeCard
+      practiceCard: this.props.initialPracticeCard,
+      openExitConfirmationDialog: false
     });
   };
 
   handleCloseOrCancel = () => {
-    // TODO: Stop user from closing if data has been changed but not saved
-    this.props.onCloseOrCancel();
+    if (this.areChangesPresent()) {
+      this.showExitConfirmationDialog();
+    } else {
+      this.props.onCloseOrCancel();
+    }
   };
 
   handleSave = () => {
@@ -48,76 +53,122 @@ export class EditPracticeItemDialog extends React.Component<Props, State> {
     });
   };
 
+  closeWithUnsavedEdits = () => {
+    this.hideExitAlert();
+    this.props.onCloseOrCancel();
+  };
+
+  showExitConfirmationDialog = () => {
+    this.setState({
+      openExitConfirmationDialog: true
+    });
+  };
+
+  hideExitAlert = () => {
+    this.setState({
+      openExitConfirmationDialog: false
+    });
+  };
+
+  areChangesPresent = (): boolean => {
+    return (
+      this.state.practiceCard.title !== this.props.initialPracticeCard.title ||
+      this.state.practiceCard.description !==
+        this.props.initialPracticeCard.description
+    );
+  };
+
+  areRequiredFieldsMissing = (): boolean => {
+    return (
+      this.state.practiceCard.title === "" ||
+      this.state.practiceCard.description === ""
+    );
+  };
+
   render() {
     return (
-      // TODO: Use provided properties for displaying title and close button
-      <Dialog
-        canEscapeKeyClose={true}
-        canOutsideClickClose={true}
-        isOpen={this.props.isOpen}
-        onOpening={this.handleDialogOpening}
-        onClose={this.handleCloseOrCancel}
-        className="dialog"
-      >
-        <Row>
-          <Col xs={10}>
-            <h1>Edit Practice Item</h1>
-          </Col>
-          <Col xs={2}>
-            <Button
-              onClick={this.handleCloseOrCancel}
-              icon={IconNames.CROSS}
-              minimal={true}
-              className="close-dialog"
-            />
-          </Col>
-        </Row>
-        <Row>
-          <Col xs={12}>
-            <h5>
+      <div>
+        {this.state && (
+          <Alert
+            isOpen={this.state.openExitConfirmationDialog}
+            cancelButtonText={"Cancel"}
+            onConfirm={this.closeWithUnsavedEdits}
+            onCancel={this.hideExitAlert}
+            intent={Intent.PRIMARY}
+            icon={IconNames.ERROR}
+          >
+            Exit with unsaved changes?
+          </Alert>
+        )}
+        // TODO: Use provided properties for displaying title and close button
+        <Dialog
+          canEscapeKeyClose={true}
+          canOutsideClickClose={true}
+          isOpen={this.props.isOpen}
+          onOpening={this.handleDialogOpening}
+          onClose={this.handleCloseOrCancel}
+          className="dialog"
+        >
+          <Row>
+            <Col xs={10}>
+              <h1>Edit Practice Item</h1>
+            </Col>
+            <Col xs={2}>
+              <Button
+                onClick={this.handleCloseOrCancel}
+                icon={IconNames.CROSS}
+                minimal={true}
+                className="close-dialog"
+              />
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={12}>
+              <h5>
+                {this.state && (
+                  <EditableText
+                    selectAllOnFocus={true}
+                    placeholder="Add title..."
+                    value={this.state.practiceCard.title}
+                    onChange={this.handleTitleChange}
+                  />
+                )}
+              </h5>
+            </Col>
+          </Row>
+          <Row className="description">
+            <Col xs={12}>
               {this.state && (
                 <EditableText
-                  selectAllOnFocus={true}
-                  placeholder="Add title..."
-                  value={this.state.practiceCard.title}
-                  onChange={this.handleTitleChange}
+                  multiline={true}
+                  minLines={15}
+                  maxLines={30}
+                  placeholder="Add description..."
+                  value={this.state.practiceCard.description}
+                  onChange={this.handleDescriptionChange}
                 />
               )}
-            </h5>
-          </Col>
-        </Row>
-        <Row className="description">
-          <Col xs={12}>
-            {this.state && (
-              <EditableText
-                multiline={true}
-                minLines={15}
-                maxLines={30}
-                placeholder="Add description..."
-                value={this.state.practiceCard.description}
-                onChange={this.handleDescriptionChange}
-              />
-            )}
-          </Col>
-        </Row>
-        <Row>
-          <Col xs={12}>
-            <div className="float-right">
-              <Button onClick={this.handleCloseOrCancel} intent={Intent.NONE}>
-                Cancel
-              </Button>
-              <Button
-                onClick={this.handleSave}
-                intent={Intent.PRIMARY}
-                icon={IconNames.FLOPPY_DISK}
-                className="save-button"
-              >
-                Save
-              </Button>
-            </div>
-          </Col>
-        </Row>
-      </Dialog>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={12}>
+              <div className="float-right">
+                <Button onClick={this.handleCloseOrCancel} intent={Intent.NONE}>
+                  Cancel
+                </Button>
+                <Button
+                  onClick={this.handleSave}
+                  intent={Intent.PRIMARY}
+                  icon={IconNames.FLOPPY_DISK}
+                  className="save-button"
+                >
+                  Save
+                </Button>
+              </div>
+            </Col>
+          </Row>
+        </Dialog>
+      </div>
     );
   }
 }
